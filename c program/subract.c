c97190ca157bd1d57ea26670923410bc47e3eb48
samikshakhobragade17 <samikshakhobragade175@gmail.com>
2022-04-02 14:03:02 +0530

    Subtract

---
+++ c program/subract.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+
+int a, b, c;
+
+printf("Enter values for a and b\n ");
+scanf("%d %d",&a,&b);
+c = a -b;
+printf("The subtraction is =%d",c);
+return 0;
+
+}
