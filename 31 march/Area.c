9e73487cc7e922ea6c9d4f607ec442d3d25ec9a3
samikshakhobragade17 <samikshakhobragade175@gmail.com>
2022-04-02 14:24:41 +0530

    Area

---
+++ 31 march/Area.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	float pi=3.14,area;
+	int radi;
+	
+	printf("enter the radius");
+	scanf("%d", &radi);
+	
+	area=pi*radi*radi;
+	
+	printf("%f",area);
+}
+	
