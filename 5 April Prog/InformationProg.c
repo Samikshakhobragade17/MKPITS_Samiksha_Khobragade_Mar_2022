445bb76d8c678428196aedda5566b8697fba8084
Samikshakhobragade17 <101171895+Samikshakhobragade17@users.noreply.github.com>
2022-04-06 11:37:50 +0530

    Name, date of birth ,mobile no. program

---
+++ 5 April Prog/InformationProg.c
@@ -0,0 +1,8 @@
+#include <stdio.h> 
+ int main()  
+  {
+     printf("Name   : Samiksha B. Khobragade\n"); 
+     printf("DOB    : March 3, 1999\n"); 
+     printf("Mobile : 123456789\n"); 
+     return(0); 
+  }
