be4bc8d9c9ddcf5ec2b513dff722ab58b64684be
Samikshakhobragade17 <101171895+Samikshakhobragade17@users.noreply.github.com>
2022-04-29 20:49:30 +0530

    function add

---
+++ c program/Function addition.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+void add()
+{
+	int n1,n2,res;
+	printf("enter 2 no");
+	scanf("%d%d",&n1,&n2);
+	res=n1+n2;
+	printf("res=%d",res);
+}
+void main()
+{
+	add();
+}
