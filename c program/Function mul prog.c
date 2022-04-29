7b070cff7d21e048f2f6c882c3491dd55f266888
Samikshakhobragade17 <101171895+Samikshakhobragade17@users.noreply.github.com>
2022-04-29 20:46:37 +0530

    function multiple

---
+++ c program/Function mul prog.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void mul()
+{
+	int n1,n2,res;
+	printf("enter 2 no");
+	scanf("%d%d",&n1,&n2);
+	res=n1*n2;
+	printf("res=%d",res);
+}
+
+void main()
+{
+	mul();
+}
