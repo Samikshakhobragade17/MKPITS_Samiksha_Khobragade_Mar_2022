aa06d286be164c26188404a45333aed98a89262e
Samikshakhobragade17 <101171895+Samikshakhobragade17@users.noreply.github.com>
2022-05-03 13:29:22 +0530

    To print the result of the specified operations

---
+++ c program/72 the result of the specified operations.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int p,q,r,s;
+	p=-1+4*6;
+	q=(35+5)%7;
+	r=14+(-4*6)/11;
+	s=2+15/6*1-7%2;
+	
+	printf("res=%d\nres=%d\nres=%d\nres=%d\n",p,q,r,s);
+}
