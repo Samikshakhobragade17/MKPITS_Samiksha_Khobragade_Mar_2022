aebb9141a15e150c8b4b18d041373e75cd4deaf8
Samikshakhobragade17 <101171895+Samikshakhobragade17@users.noreply.github.com>
2022-05-10 20:50:06 +0530

    Type 1 no argument and no return type function prog

---
+++ c program/Type 1 prog 10.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void fun()
+{
+	int n1,n2,n3,mul;
+	printf("enter 1st no");
+	scanf("%d",&n1);
+	printf("enter 2nd no");
+	scanf("%d",&n2);
+	printf("enter 3rd no");
+	scanf("%d",&n3);
+	
+	mul=n1*n2*n3;
+	
+	printf("multiplication of nos %dn%d%d=%d",n1,n2,n3,mul);
+	
+}
+void main()
+{
+	fun();
+}
