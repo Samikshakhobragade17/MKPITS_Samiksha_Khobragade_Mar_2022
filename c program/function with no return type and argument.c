2324ab1403bbd821baac81d5353f625ecb121c7a
Samikshakhobragade17 <101171895+Samikshakhobragade17@users.noreply.github.com>
2022-05-11 13:12:10 +0530

    structure program

---
+++ c program/function with no return type and argument.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+struct student
+{
+	int id;
+	char name[20];
+	
+}s1;
+void fun(struct student s2)
+{
+printf("student id: %d\n",s2.id);
+printf("student name: %s",s2.name);	
+}
+void main()
+{
+	printf("enter student id: ");
+	scanf("%d",&s1.id);
+	printf("enter student name: ");
+	scanf("%s",s1.name);
+	fun(s1);
+}
