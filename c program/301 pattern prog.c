90eb53b57b253fa354282dce74f234d81979a832
Samikshakhobragade17 <101171895+Samikshakhobragade17@users.noreply.github.com>
2022-05-11 12:40:16 +0530

    pattern program

---
+++ c program/301 pattern prog.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int r,c,add=0;
+	for(r=5;r>=1;r--)
+	{
+		for(c=1;c<=r;c++)
+		{
+		add=add+1;	
+		printf("%c",(add+64));
+		}
+		add=0;
+		printf("\n");
+	}
+	
+}
