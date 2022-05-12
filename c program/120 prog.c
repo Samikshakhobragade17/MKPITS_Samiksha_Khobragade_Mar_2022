343750c9b3e4ffddeb1c823b53b927c9f46f762f
Samikshakhobragade17 <101171895+Samikshakhobragade17@users.noreply.github.com>
2022-05-12 20:51:57 +0530

    To compute the sum of the numbers in a given array except those numbers starting with 5 followed by atleast one 6

---
+++ c program/120 prog.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	int i,sum=0,a[7];
+	printf("Enter the array element\n");
+	for(i=0;i<7;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<7;i++)
+	{
+		if(a[i]!=5&&a[i+1]!=6)
+		{
+		sum=sum+a[i];
+		}
+		
+	}
+	printf("Sum of values in array those starting with 5 followed by 6: %d",sum-6);
+}
