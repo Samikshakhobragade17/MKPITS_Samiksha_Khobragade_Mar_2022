9bbcfed0ceaeecef658217309fa1026442f6e2d5
Samikshakhobragade17 <101171895+Samikshakhobragade17@users.noreply.github.com>
2022-05-13 21:01:34 +0530

    To check if the number of 3's is greater than the number of 5's

---
+++ c program/124 prog.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	int i,a[10],size,c3=0,c5=0;
+	printf("Enter the size of array: ");
+	scanf("%d",&size);
+	printf("Enter array element\n");
+	for(i=0;i<size;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<size;i++)
+	{
+		if(a[i]==3)
+		{
+		c3=c3+1;	
+		}
+		else if(a[i]==5)
+		{
+			c5=c5+1;
+		}
+		else
+		{
+			
+		}
+	}
+	if(c3>c5)
+	{
+		printf("Numbers of 3's are greater than numbers of 5's");
+	}
+	else
+	{
+		printf("Numbers of 3's are not greater than numbers of 5's");
+		
+    }
+	
+}
