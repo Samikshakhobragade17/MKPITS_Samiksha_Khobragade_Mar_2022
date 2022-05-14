1020bd38d92cc8379a645db0df8c44574c21f744
Samikshakhobragade17 <101171895+Samikshakhobragade17@users.noreply.github.com>
2022-05-14 20:36:54 +0530

    pattern program

---
+++ c program/321 prog.c
@@ -0,0 +1,24 @@
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	int r,c;
+	for(r=1;r<=4;r++)
+	{
+		for(c=1;c<=4;c++)
+		{
+			if(r+c>4)
+			{
+				printf("* ");
+				
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+}
